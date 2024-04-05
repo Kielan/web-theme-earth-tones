c56aa0169920d373d58d15766c4e833835a4b35b
Kielan <ki.lemons@gmail.com>
2024-04-05 11:08:07 -0700

    Create Navbar.js

---
+++ components/Navbar.js
@@ -0,0 +1,150 @@
+<nav id="navbar" class="tw-w-full tw-px-4 tw-mx-auto tw-fixed tw-inset-x-0 tw-top-0 tw-z-20 tw-max-w-7xl md:tw-px-6 lg:tw-px-8 remotive-bg-light" style="">
+<div class="tw-items-center tw-justify-between md:tw-flex">
+<div class="tw-flex tw-items-center tw-justify-between tw-h-16">
+<a class="tw-flex-shrink-0 nav-active" aria-label="Homepage" href="/">
+<img src="/remotive_website_layout/static/src/brand/SVG/wordmark_H_orange.svg" width="220" height="55" draggable="false" class="tw-w-auto tw-h-12" alt="">
+</a>
+<div class="tw-flex tw-items-center md:tw-hidden">
+<button id="nav-burger" aria-label="Menu" class="tw-inline-flex tw-items-center tw-justify-center tw-p-2 remotive-btn-chocolate focus:tw-outline-none">
+<svg id="nav-burger-closed" class="tw-w-6 tw-h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+</svg>
+<svg id="nav-burger-opened" class="tw-hidden tw-w-6 tw-h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+</svg>
+</button>
+</div>
+</div>
+<div class="tw-hidden tw-block" id="nav-mobile" style="height: 100vh;">
+<div class="tw-mx-auto tw-mt-12">
+<div>
+<span class="h2 tw-block tw-px-1 tw-pt-1 tw-leading-5 remotive-text-default-color">
+Find remote jobs
+</span>
+</div>
+<div class="tw-font-normal tw-mt-2">
+<div class="tw-py-1 md:tw-bg-white md:tw-rounded-md md:tw-shadow-xs" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
+<a href="/remote-jobs" class="tw-block tw-px-1 tw-py-2 tw-leading-5 remotive-text-default-color remotive-url focus:tw-outline-none">
+Search by skill &amp; location
+</a>
+<a href="/remote-companies" class="tw-block tw-px-1 tw-py-2 tw-leading-5 remotive-text-default-color remotive-url focus:tw-outline-none">
+Search by company
+</a>
+<a href="/remote-jobs/rss-feed" class="tw-block tw-px-1 tw-py-2 tw-leading-5 remotive-text-default-color remotive-url focus:tw-outline-none">
+RSS Feeds
+</a>
+<a href="/remote-jobs/api" class="tw-block tw-px-1 tw-py-2 tw-leading-5 remotive-text-default-color remotive-url focus:tw-outline-none">
+Public API
+</a>
+<a href="/accelerator" class="tw-block tw-px-1 tw-py-2 tw-leading-5 remotive-text-default-color remotive-url focus:tw-outline-none">
+Join Remotive Accelerator
+</a>
+<a role="button" href="/accelerator" class="tw-block tw-text-center tw-mb-4 tw-mt-2 tw-px-2 tw-py-2 remotive-btn-chocolate">
+<span class="tw-mr-auto">Unlock 10x more jobs</span>
+</a>
+<a role="button" href="/web/login" class="tw-block tw-text-center tw-mb-4 tw-mt-2 tw-px-2 tw-py-2 remotive-btn-transparent">
+<span class="tw-mr-auto">Log in</span>
+</a>
+</div>
+</div>
+</div>
+<div class="tw-mx-auto tw-mt-12">
+<div>
+<span class="h2 tw-block tw-px-1 tw-pt-1 tw-leading-5 remotive-text-default-color">
+Resources
+</span>
+</div>
+<div class="tw-font-normal tw-mt-2">
+<div class="tw-py-1 md:tw-bg-white md:tw-rounded-md md:tw-shadow-xs" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
+<a href="/blog/tag/job-search-tips/" class="tw-block tw-px-1 tw-py-2 tw-leading-5 remotive-text-default-color remotive-url focus:tw-outline-none">
+Job Search Tips
+</a>
+<a href="/webinar" class="tw-block tw-px-1 tw-py-2 tw-leading-5 remotive-text-default-color remotive-url focus:tw-outline-none">
+Webinar
+</a>
+<a href="/blog/digital-nomad-visas/" class="tw-block tw-px-1 tw-py-2 tw-leading-5 remotive-text-default-color remotive-url focus:tw-outline-none">
+Digital Nomad Visas
+</a>
+<a href="/about" class="tw-block tw-px-1 tw-py-2 tw-leading-5 remotive-text-default-color remotive-url focus:tw-outline-none" style="white-space: nowrap;">
+About us
+</a>
+</div>
+</div>
+</div>
+<div class="tw-mx-auto tw-mt-12">
+<div class="tw-font-normal tw-mt-2">
+<div class="tw-py-1 md:tw-bg-white md:tw-rounded-md md:tw-shadow-xs" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
+<a role="button" href="/hire" class="tw-block tw-text-center tw-mb-4 tw-mt-2 tw-px-2 tw-py-2 remotive-btn-transparent">
+<span class="tw-mr-auto">For Employers</span>
+</a>
+</div>
+</div>
+</div>
+</div>
+<div class="remotive-bold tw-items-center tw-hidden md:tw-flex">
+<div class="md:tw-relative md:tw-ml-3 tw-my-2" id="nav-applicants-link">
+<div class="tw-flex" style="align-items: center">
+<a href="#nav-applicants-link" class="remotive-dropdown-menu tw-block tw-px-1 tw-pt-1 tw-text-base tw-leading-5 remotive-text-default-color remotive-url focus:tw-outline-none">
+Find remote jobs ⌄
+</a>
+</div>
+<div class="tw-font-normal tw-right-0 tw-hidden tw-mt-2 tw-origin-top-right md:tw-absolute md:tw-rounded-md md:tw-shadow-lg" id="nav-applicants-dropdown">
+<div class="tw-py-1 md:tw-bg-white md:tw-rounded-md md:tw-shadow-xs" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
+<a href="/remote-jobs" class="tw-block tw-px-4 tw-py-2 tw-leading-5 remotive-text-default-color remotive-url focus:tw-outline-none" style="white-space: nowrap;">
+Search by skill &amp; location
+</a>
+<a href="/remote-companies" class="tw-block tw-px-4 tw-py-2 tw-leading-5 remotive-text-default-color remotive-url focus:tw-outline-none" style="white-space: nowrap;">
+Search by company
+</a>
+<a href="/remote-jobs/rss-feed" class="tw-block tw-px-4 tw-py-2 tw-leading-5 remotive-text-default-color remotive-url focus:tw-outline-none" style="white-space: nowrap;">
+RSS Feeds
+</a>
+<a href="/remote-jobs/api" class="tw-block tw-px-4 tw-py-2 tw-leading-5 remotive-text-default-color remotive-url focus:tw-outline-none" style="white-space: nowrap;">
+Public API
+</a>
+<a href="/accelerator" class="tw-block tw-px-4 tw-py-2 tw-leading-5 remotive-text-default-color remotive-url focus:tw-outline-none" style="white-space: nowrap;">
+Join Remotive Accelerator
+</a>
+<a role="button" href="/accelerator" class="remotive-only-public tw-block tw-text-center tw-mb-4 tw-mt-2 tw-ml-4 tw-mr-4 tw-px-2 tw-py-2 remotive-btn-chocolate">
+<span class="tw-mr-auto">Unlock 10x more jobs</span>
+</a>
+</div>
+</div>
+</div>
+<div class="md:tw-relative md:tw-ml-3">
+<div class="tw-flex" style="align-items: center" id="nav-resources-link">
+<a href="#nav-resources-link" class="remotive-dropdown-menu tw-block tw-px-1 tw-pt-1 tw-text-base tw-leading-5 remotive-text-default-color remotive-url focus:tw-outline-none">
+Resources ⌄
+</a>
+</div>
+<div class="tw-font-normal tw-right-0 tw-hidden tw-w-48 tw-mt-2 tw-origin-top-right md:tw-absolute md:tw-rounded-md md:tw-shadow-lg" id="nav-resources-dropdown">
+<div class="tw-py-1 md:tw-bg-white md:tw-rounded-md md:tw-shadow-xs" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
+<a href="/blog/tag/job-search-tips/" class="tw-block tw-px-4 tw-py-2 tw-leading-5 remotive-text-default-color remotive-url focus:tw-outline-none">
+Job Search Tips
+</a>
+<a href="/webinar" class="tw-block tw-px-4 tw-py-2 tw-leading-5 remotive-text-default-color remotive-url focus:tw-outline-none">
+Webinar
+</a>
+<a href="/blog/digital-nomad-visas/" class="tw-block tw-px-4 tw-py-2 tw-leading-5 remotive-text-default-color remotive-url focus:tw-outline-none">
+Digital Nomad Visas
+</a>
+<a href="/about" class="tw-block tw-px-4 tw-py-2 tw-leading-5 remotive-text-default-color remotive-url focus:tw-outline-none" style="white-space: nowrap;">
+About us
+</a>
+</div>
+</div>
+</div>
+<div class="tw-flex tw-mt-4 tw-mb-4 tw-text-center md:tw-ml-8 md:tw-my-0">
+<a role="button" href="/my" class="remotive-only-private tw-block tw-px-4 tw-py-2 remotive-btn-transparent tw-mr-2">
+<span class="tw-mr-auto">My Account</span>
+</a>
+<a role="button" href="/web/login" class="remotive-only-public tw-block tw-px-4 tw-py-2 remotive-btn-transparent tw-mr-2">
+<span class="tw-mr-auto">Log in</span>
+</a>
+<a role="button" href="/hire" class="tw-block tw-px-4 tw-py-2 remotive-btn-transparent">
+For Employers
+</a>
+</div>
+</div>
+</div>
+</nav>
